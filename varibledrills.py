e545a0a0a8db9d75e1a428c5d2b42c7b47dabbb3
anna463 <57601829+anna463@users.noreply.github.com>
2019-11-25 19:58:09 -0600

    Add files via upload

---
+++ varibledrills.py
@@ -0,0 +1,88 @@
+'''
+Created on Nov 25, 2019
+
+@author: ITAUser
+'''
+from symbol import annassign
+from turtle import done
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+name = "Michael"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''INTEGERS'''
+#1) Make a variable called var1 that is set to 5. And print the variable.
+var1 = 5
+print (var1)
+#2) Make a variable called var2 that is set to 10. And print the variable.
+var2 = 10
+print (var2)
+#3) Make a variable called var3 that is set to var2. And print the variable.
+var3 = var2
+print (var3)
+#4) Make a variable called x that is set to 24. And print the variable.
+varx = 24
+print (varx)
+#5) Make a variable called y that is set to 100. And print the variable.
+vary = 100
+print (vary)
+'''FLOATS'''
+#6) Make a variable called z that is set to 1.1. And print the variable.
+varz = 1.1
+print (varz)
+#7) Make a variable called a that is set to 2.75. And print the variable.
+vara = 2.75
+print (vara)
+#8) Make a variable called b that is set to a. And print the variable.
+varb = a
+print (varb)
+#9) Make a variable called c that is set to 100.1. And print the variable.
+varc = 100.1
+print (varc)
+#10) Make a variable called d that is set to 12.3. And print the variable.
+vard = 12.3
+print (vard)
+'''STRINGS'''
+#11) Make a variable called name that is set to your name. And print the variable.
+varname =" Anna"
+print (varname)
+#12) Make a variable called e that is set to the first letter of your name. And print the variable.
+vare = "a"
+print (vare)
+#13) Make a variable called f that is set to the last letter of your name. And print the variable.
+varf = "m"
+print (varf)
+#14) Make a variable called g that is set to "chicken". And print the variable.
+varg = "chicken"
+print (varg)
+#15) Make a variable called h that is set to the first 3 letters of you name, using the string indexes. And print the variable.
+varh = "ann"
+print (varh)
+'''BOOLEANS'''
+#16) Make a variable called done that is set to False. And print the variable.
+vardone = False
+print (vardone)
+#17) Make a variable called smart that is set to True. And print the variable.
+varsmart = "true "
+print (varsmart)
+#18) Make a variable called i that is set to done. And print the variable.
+vari = done
+print (vari)
+#19) Make a variable called j that is set to False. And print the variable.
+varj = "false"
+print (varj)
+#20) Make a variable called almostDone that is set to True. And print the variable.
+varalmostdone = "true"
+print (varalmostdone)
+
